69daac047d18f160f9f799a7e203f13cf8843487
Akshat0102 <76106653+Akshat0102@users.noreply.github.com>
2022-01-12 19:50:28 +0530

    added the logic of the app

--- app/src/main/java/com/example/billsplitter/MainActivity.kt
+++ app/src/main/java/com/example/billsplitter/MainActivity.kt
@@ -2,10 +2,47 @@ package com.example.billsplitter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.billsplitter.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.splitButton.setOnClickListener { splitBill() }
+    }
+
+    private fun splitBill() {
+
+        val bill = binding.billValueEditText.text.toString().toDoubleOrNull()
+
+        val friends = binding.numberOfFriendsEditText.text.toString().toIntOrNull()
+
+        if (bill == null) {
+            Toast.makeText(this, "Bill can't be empty", Toast.LENGTH_SHORT).show()
+            return
+        } else if (bill == 0.0) {
+            Toast.makeText(this, "Bill can't be zero", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        if (friends == null) {
+            Toast.makeText(this, "Number of friends can't be empty", Toast.LENGTH_SHORT).show()
+            return
+        } else if (friends == 0) {
+            Toast.makeText(this, "Number of friends can't be zero or decimal", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val amount = bill / friends
+
+        val formatAmount = NumberFormat.getCurrencyInstance().format(amount)
+        binding.amountPerPerson.text = getString(R.string.amount_per, formatAmount)
     }
 }
